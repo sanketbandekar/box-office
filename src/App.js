d79eb2d094e3d72786cf75da78d6b04eb3065f64
sanket <bandekarsanket03@gmail.com>
2021-05-26 12:33:48 +0530

    added page components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
+    <div>
+      <Nav/>
+    
     <Switch>
       <Route exact path="/">
-        This is Home Page.
+        <Home/>
       </Route>
       <Route exact path="/starred">
-        This is Starred Page.
+        <Starred/>
       </Route>
       <Route>
+        <div>
         Error 404 page not found.
+        </div>
       </Route>
     </Switch>
+    </div>
   );
 }
 
