6ff57f2e7106b18e4ebd0c473bcf94e1f6683187
sanket <bandekarsanket03@gmail.com>
2021-05-27 13:49:23 +0530

    added styled components

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,6 +6,7 @@ import Details from '../components/show/Details'
 import Seasons from '../components/show/Seasons'
 import ShowMainData from '../components/show/ShowMainData'
 import { apiGet } from '../misc/config'
+import { InfoBlock, ShowPageWrapper } from './Show.styled'
 
 const reducer = (prevState, action) => {
 
@@ -60,7 +61,6 @@ const Show = () => {
         } )
     }, [id] )
 
-     console.log('show', show);
 
     if(isLoading){
         return <div>Div is loading</div>
@@ -74,7 +74,7 @@ const Show = () => {
 
 
         return (
-            <div>
+            <ShowPageWrapper>
                 <ShowMainData 
                 image={show.image}
                 name={show.name}
@@ -82,24 +82,24 @@ const Show = () => {
                 summary={show.summary}
                 tags = {show.genres} />
 
-                <div>
+                <InfoBlock>
                     <h2>Details</h2>
                     <Details
                     status={show.status}
                     network={show.network}
                     premiered={show.premiered}/>
-                </div>
-                <div>
+                </InfoBlock>
+                <InfoBlock>
                     <h2>Seasons</h2>
                     <Seasons 
                     seasons ={show._embedded.seasons}/>
-                </div>
-                <div>
+                </InfoBlock>
+                <InfoBlock>
                     <h2>Cast</h2>
                     <Cast
                     cast={show._embedded.cast}/>
-                </div>
-            </div>
+                </InfoBlock>
+            </ShowPageWrapper>
         )
     }
 
