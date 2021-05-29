2882687d132e13a229c85cd111987cdd08cc10b6
sanket <bandekarsanket03@gmail.com>
2021-05-29 10:32:10 +0530

    created two new custom hooks

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,77 +1,30 @@
 /* eslint-disable no-underscore-dangle */
-import React,{ useEffect, useReducer } from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom'
 import Cast from '../components/show/Cast'
 import Details from '../components/show/Details'
 import Seasons from '../components/show/Seasons'
 import ShowMainData from '../components/show/ShowMainData'
-import { apiGet } from '../misc/config'
+import { useShow } from '../misc/custom-hooks'
 import { InfoBlock, ShowPageWrapper } from './Show.styled'
 
-const reducer = (prevState, action) => {
 
-    switch (action.type) {
-        case 'FETCH_SUCCESS':{
-            return {...prevState, isLoading: false, error: null, show: action.show}
-        }
 
-        case 'FETCH_FAILED' :{
-            return{...prevState , isLoading:false, error : action.error}
-        }
-            
-            
-    
-        default: return prevState;
-    }
-}
-
-const intitialState = {
-    show: null,
-    isLoading: true,
-    error: null
-}
 
 const Show = () => {
 
     const {id} = useParams();
 
-    const[{show, isLoading, error}, dispatch] =useReducer(reducer,intitialState)
-    
-    // const [show, setShow] = useState(null);
-    // const [isLoading, setIsLoading] = useState(true);
-    // const [error, setError] = useState(null);
-
-    useEffect( () => {
-
-        let isMounted = true;
-
-        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then( (results) =>{
-
-            if(isMounted){
-                dispatch({ type: 'FETCH_SUCCESS', show: results })
-            }
-        } ).catch( err => {
-            if(isMounted){
-                dispatch({ type: 'FETCH_Failed', error: err.message })
-            }
-        });
-
-        return ( () => {
-            isMounted = false;
-        } )
-    }, [id] )
-
+    const { show, isLoading, error } = useShow(id)
 
     if(isLoading){
         return <div>Div is loading</div>
     }
-
     if(error){
         return <div>
             {error}
         </div>
     }
-
 
         return (
             <ShowPageWrapper>
